b2097f9b79affaeb24b06f8c6cc963a17d587526
Jake Cattrall <krazyjakee@gmail.com>
2022-11-21 02:15:00 +0000

    final controls

--- PilotCamera.gd
+++ PilotCamera.gd
@@ -11,40 +11,50 @@ extends CharacterBody3D
 
 const SPEED = 0.3
 const ACCELERATION = 0.5
-const SENSITIVITY = 0.01
-const MOUSE_PULL_CAP = 0.01
+const ROLL_SPEED = 0.1
+const SENSITIVITY = 0.05
 const DECELERATION = 3.0
+const TURN_SPEED = 0.1
+
+var angular_velocity: Quaternion
 
 func _input(event: InputEvent):
   if pilot_active:
     if event is InputEventMouseMotion:
       # Grab the mouse input
-      mouse_axis.y += clamp(event.relative.x * SENSITIVITY, -MOUSE_PULL_CAP, MOUSE_PULL_CAP)
-      mouse_axis.x += clamp(event.relative.y * SENSITIVITY, -MOUSE_PULL_CAP, MOUSE_PULL_CAP)
+      mouse_axis.y = fmod(mouse_axis.y - clamp(event.relative.y * SENSITIVITY, -TURN_SPEED, TURN_SPEED), 360)
+      mouse_axis.x = fmod(mouse_axis.x - clamp(event.relative.x * SENSITIVITY, -TURN_SPEED, TURN_SPEED), 360)
 
 func _physics_process(delta):
+  var direction = (transform.basis * Vector3(0, 0, 0)).normalized()
   if pilot_active and gui.visible == false:
+    
     # Grab Q and E input for barrel roll
-    if Input.is_action_pressed("pilot_roll_left"): roll_rotation = ACCELERATION
-    if Input.is_action_pressed("pilot_roll_right"): roll_rotation = -ACCELERATION
+    if Input.is_action_pressed("pilot_roll_left"): roll_rotation = ROLL_SPEED
+    if Input.is_action_pressed("pilot_roll_right"): roll_rotation = -ROLL_SPEED
     
-    # Rotate the camera objects in the same way as the player is set up (With X axis object being the camera itself).
-    outside_camera_3d.rotate_object_local(Vector3(1, 0, 0), -mouse_axis.x * delta)
-    rotate_object_local(Vector3(0, 1, 0), -mouse_axis.y * delta)
-    rotate_object_local(Vector3(0, 0, 1), roll_rotation * delta)
-    distant_camera_3d.rotate_object_local(Vector3(1, 0, 0), -mouse_axis.x * delta)
-    distant_camera.rotate_object_local(Vector3(0, 1, 0), -mouse_axis.y * delta)
-    distant_camera.rotate_object_local(Vector3(0, 0, 1), roll_rotation * delta)
+    # The magic math to point us in the right direction
+    var rotation_vec = Vector3(deg_to_rad(mouse_axis.y), deg_to_rad(mouse_axis.x), roll_rotation) * delta
+    var relative_velocity = Quaternion(rotation_vec)
+    angular_velocity = (angular_velocity * relative_velocity).slerp(Quaternion.IDENTITY, 0.15)
+    var current_rotation = Quaternion(transform.basis)
+    var final_rotation = current_rotation * angular_velocity
+    transform.basis = Basis(final_rotation)
+    transform = transform.orthonormalized()
     
-    # Implement deceleration
+    # Apply the same transform to the distant camera
+    distant_camera.transform = transform
+    
+    # Decelerate
     mouse_axis.x = lerp(mouse_axis.x, 0.0, DECELERATION * delta)
     mouse_axis.y = lerp(mouse_axis.y, 0.0, DECELERATION * delta)
     roll_rotation = lerp(roll_rotation, 0.0, DECELERATION * delta)
     
     # Perform movement in 3D space
     var input_dir = Input.get_vector("left", "right", "up", "down")
-    var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-    velocity.x = lerp(velocity.x, direction.x * SPEED, ACCELERATION * delta)
-    velocity.y = lerp(velocity.y, direction.y * SPEED, ACCELERATION * delta)
-    velocity.z = lerp(velocity.z, direction.z * SPEED, ACCELERATION * delta)
-    move_and_slide()
+    direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+    
+  velocity.x = lerp(velocity.x, direction.x * SPEED, ACCELERATION * delta)
+  velocity.y = lerp(velocity.y, direction.y * SPEED, ACCELERATION * delta)
+  velocity.z = lerp(velocity.z, direction.z * SPEED, ACCELERATION * delta)
+  move_and_slide()
